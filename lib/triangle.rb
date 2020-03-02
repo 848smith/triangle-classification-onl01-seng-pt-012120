86fd2f64b36c8284e111857aff15d6f3e1b145e1
Andrew Smith <848smith@gmail.com>
2020-03-02 03:27:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,14 @@
 class Triangle
   def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  
+  def kind
     
+  end
+  
+  class TriangleError < StandardError
   end
 end
